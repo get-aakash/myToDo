a97995bb2905549a03d08a8448bd8b42b630c08a
aakash <mail.aakash108@gmail.com>
2022-04-25 20:34:19 +1000

    styling the footer

--- src/MyComponents/ToDos.js
+++ src/MyComponents/ToDos.js
@@ -3,12 +3,19 @@ import ToDoItem from "./ToDoItem";
 
 
 const ToDos = (props) => {
+    let myStyle = {
+        minHeight:'70vh',
+        margin:"40px auto"
+    }
     return (
-        <div className='container'>
+        <div className='container ' style={myStyle}>
             <h3 className=' my-3'>ToDos List</h3>
             {props.todo.length===0?"No ToDos to display":props.todo.map((todo)=>{
-                return <ToDoItem todo={todo} key={todo.sn} onDelete={props.onDelete}/>
-            })}
+                return (
+                <>
+                <ToDoItem todo={todo} key={todo.sn} onDelete={props.onDelete}/><hr/>
+                </>
+            )})}
             
             
         </div>
