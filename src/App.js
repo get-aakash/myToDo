051a5997f83bd3dcbca8e61f22eb6b6e299705dc
aakash <mail.aakash108@gmail.com>
2022-04-29 14:27:36 +1000

    adding the local storage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from "./MyComponents/Footer";
 import AddTodo from "./MyComponents/AddTodo";
 import ToDos from "./MyComponents/ToDos";
 import About from './MyComponents/About';
-import React , { useState } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,55 +12,71 @@ import {
 } from "react-router-dom";
 
 function App() {
-  const onDelete = (todo)=>{
-    console.log("this is onDelete",todo)
-  
-  setTodos(todos.filter((e)=>{
-    return e!==todo;
-  }))}
-
-  const addTodo = (title, desc)=>{
-    console.log("i am adding the todo",title,desc)
-    let sn;
-    if (todos.length===0){
-      sn=0;
-    }
-    else{
-       sn = todos[todos.length-1].sn +1;
-
-    }
-    
-    const  myToDo= {
-      sn:sn,
-      title:title,
-      desc:desc
-    }
-    setTodos([...todos,myToDo]);
-    console.log(myToDo)
+  let initTodo;
+  if (localStorage.getItem('todos') === null) {
+    initTodo = []
   }
-  const [todos, setTodos] = useState( []);
+  else {
+    initTodo = JSON.parse(localStorage.getItem('todos'))
+  }
+  const onDelete = (todo) => {
+    console.log("this is onDelete", todo)
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("i am adding the todo", title, desc)
+    let sn;
+    if (todos.length === 0) {
+      sn = 0;
+    }
+    else {
+      sn = todos[todos.length - 1].sn + 1;
+
+    }
+
+    const myToDo = {
+      sn: sn,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos, myToDo]);
+    console.log(myToDo);
+  }
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+  }, [todos])
+
+
+
   return (
     <>
-    <Router>
-      <Header title="My Todos List" searchBar={false} />
-      <Switch>
-      <Route exact path="/" render={()=>{
-        return(
-          <>
-          <AddTodo addTodo={addTodo}/>
-          <ToDos todo={todos} onDelete={onDelete}/>
-          </>
-        )
-      }}>
-          
+      <Router>
+        <Header title="My Todos List" searchBar={false} />
+        <Switch>
+          <Route exact path="/" render={() => {
+            return (
+              <>
+                <AddTodo addTodo={addTodo} />
+                <ToDos todo={todos} onDelete={onDelete} />
+              </>
+            )
+          }}>
+
           </Route>
           <Route path="/about">
             <About />
           </Route>
-      </Switch>
-      
-      <Footer />
-    </Router>
+        </Switch>
+
+        <Footer />
+      </Router>
     </>
 
   );
